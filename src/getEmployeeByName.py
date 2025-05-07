59e97bf99d8d231b7edabb431f85c69903f15761
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-05-07 19:01:47 -0300

    feat: adiciona código da função get_employee_by_name

--- src/getEmployeeByName.py
+++ src/getEmployeeByName.py
@@ -1,10 +1,10 @@
-from data import zoo_data
+from zoo_data import data
 
 def get_employee_by_name(employee_name):
     if not employee_name:
         return []
 
-    for employee in zoo_data['employee']:
+    for employee in data['employees']:
         if employee['firstName'] == employee_name:
             return [employee]
         elif employee['lastName'] == employee_name:
@@ -14,3 +14,4 @@ def get_employee_by_name(employee_name):
         return []
 
 print(get_employee_by_name('Stephanie'))
+print(get_employee_by_name(''))
