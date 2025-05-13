3cbc3d75c3ffeca0c8f000a4fb312dfac17d1d6b
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-05-13 09:35:07 -0300

    feat:  adiciona função get_oldest_from_first_species para buscar por informações do animal mais velho da primeira espécie gerenciada por um funcionário usando o seu id.

--- src/getAnimalsOlderThan.py
+++ src/getAnimalsOlderThan.py
@@ -1,3 +1,10 @@
-def get_animals_older_than(animal, age):
-    # seu código aqui
-    pass
+from zoo_data import data
+
+def get_animals_older_than(species_name, min_age):
+    for species in data['species']:
+        if species['name'] == species_name:
+            return all(resident['age'] >= min_age for resident in species['residents'])
+    return False
+
+print(get_animals_older_than('lions', 6))
+print(get_animals_older_than('tigers', 18))
