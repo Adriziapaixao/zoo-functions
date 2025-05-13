bb70d9d36c7821d97bef13fa2d76b718913847a2
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-05-13 09:31:23 -0300

    feat: adiciona função count_animals para contar animais por espécie e sexo

--- src/countAnimals.py
+++ src/countAnimals.py
@@ -1,3 +1,18 @@
+from zoo_data import data
 def count_animals(animal=None):
-    # seu código aqui
-    pass
+    if animal is None:
+        return {species['name']: len(species['residents']) for species in data['species']}
+
+    specie_name = animal.get('specie')
+    species = next((s for s in data['species'] if s["name"] == specie_name), None)
+
+    if not species:
+        return 0
+
+    if 'sex' in animal:
+        return len([resident for resident in species['residents'] if resident['sex'] == animal['sex']])
+    return len(species['residents'])
+
+print(count_animals())
+print(count_animals({'specie': 'giraffes'}))
+print(count_animals({'specie': 'penguins', 'sex': "female"}))
